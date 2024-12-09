d2fb0057425a365ac3d489776bdbcaa3c38414d5
Zefir22804 <varlamovkostyaa30@gmail.com>
2024-12-09 19:36:11 +0300

    Pyhton combat 2.0
    New version
    
    + 2 new cards
    + no connecting to server

--- version 1.0/Game.py
+++ version 1.0/Game.py
@@ -1,10 +1,12 @@
-import time
+
 
 earcli = 0
 balance = 100
 pcgshop = 100
 pgeshop = 200
 pcrshop = 10
+Lbfshop = 40
+pcushop = 20
 cards = 0
 
 print("Pyhton combat")
@@ -17,55 +19,57 @@ while True:
         print("Youre got +" , + earcli , "click")
         balance = balance + earcli
     if command1 == 'earn':
-        print("Connect to server...")
-        time.sleep(1)
         print("======EARN STATUS======")
-        time.sleep(0.2)
         print("click earn = " , earcli)
-        time.sleep(0.4)
         print("youre balance = " , balance , "points")
-        time.sleep(0.2)
         print("Cards purchased - " , cards)
     if command1 == 'shop':
-        print("Connect to server...")
-        time.sleep(1.5)
         print("======SHOP======")
-        time.sleep(0.2)
         print("pyhton click game - " , pcgshop)
-        time.sleep(0.5)
         print("pyhton game engene - " , pgeshop)
-        time.sleep(0.3)
         print("Pyhton combat realise - " , pcrshop)
+        print("Litle bug fix - " , Lbfshop)
+        print("Pyhton Combat update - " , pcushop)
     if command1 == 'buy_pcg':
-        print("Connecting to server...")
         cards = cards + 1
         balance = balance - pcgshop
         earcli = earcli + 10
         pcgshop = pcgshop + 10
-        time.sleep(0.3)
         print("Card purchased successfully")
         print("Click earn - " , earcli)
         print("Cards purchased - " , cards)
     if command1 == 'buy_pge':
-        print("Connecting to server...")
-        time.sleep(0.2)
         balance = balance - pgeshop
         earcli = earcli + 20
         pgeshop = pgeshop + 1
-        time.sleep(0.3)
         print("Card purchased successfully")
         print("Click earn - " , earcli)
         print("Cards purchased - " , cards)
     if command1 == 'buy_pcr':
-        print("Connecting to server...")
-        time.sleep(0.2)
         cards = cards + 1
         balance = balance - pcrshop
         earcli = earcli + 1
         pcrshop = pcrshop + 1
-        time.sleep(0.3)
         print("Card purchased successfully")
         print("Click earn - " , earcli)
         print("Cards purchased - " , cards)
+    if command1 == "buy_lbf"
+        cards = cards + 1
+        balance = balance - Lbfshop
+        earcli = earcli + 50
+        Lbfshop = Lbfshop + 5
+        print("Card purchased successfully")
+        print("Click earn - " , earcli)
+        print("Cards purchased - " , cards)
+    if command1 == 'buy_pcu'
+        cards = cards + 1
+        balance = balance - pcushop
+        earcli = earcli + 50
+        pcushop = pcushop + 2
+        print("Card purchased successfully")
+        print("Click earn - " , earcli)
+        print("Cards purchased - " , cards)
+
+
         
         
